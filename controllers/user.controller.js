0020ecaef75dd858ced8928e2240f77af351d913
Abhirag <abhirag4169@gmail.com>
2024-08-05 15:44:57 +0530

    ...

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -35,6 +35,7 @@ export const register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 3600000,
+      sameSite: 'None',
     });
 
     res.status(201).json({ user: savedUser, token });
@@ -69,6 +70,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true, // 'None' if cross-site
       maxAge: 3600000,
+      sameSite: 'None',
     });
     
 
@@ -83,7 +85,7 @@ export const login = async (req, res) => {
 export const logout = async(req, res)=>{
   const options = {
     httpOnly: true,
-    secure: true
+    sameSite: 'None',
 }
 
  return res
