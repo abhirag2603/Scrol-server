cf891dec893c587e6b6432b42f216de97dcab14d
Abhirag <abhirag4169@gmail.com>
2024-08-13 00:34:22 +0530

    delete post controller and route added

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -94,3 +94,25 @@ export const likePost =async(req,res)=>{
     }
 }
 
+export const deletePost = async (req, res) => {
+    try {
+      const { postId } = req.body;
+  
+      if (!postId) {
+        return res.status(400).json({ message: 'Post ID is required' });
+      }
+  
+      const deletedPost = await Post.findByIdAndDelete(postId);
+  
+      if (!deletedPost) {
+        console.log('Post not found');
+        return res.status(404).json({ message: 'Post not found' });
+      }
+  
+      res.status(200).json('Post deleted successfully');
+    } catch (error) {
+      console.error('Error deleting post:', error.message);
+      res.status(500).json({ message: 'Server error' });
+    }
+  };
+  
--- routes/post.route.js
+++ routes/post.route.js
@@ -2,12 +2,12 @@ import {Router} from 'express'
 const router = Router()
 import { verifyJWT } from '../middlewares/auth.middleware.js'
 import { upload } from '../middlewares/multer.middleware.js';
-import { createPost, getFeedPosts, getUserPosts,likePost} from '../controllers/post.controller.js';
+import { createPost, deletePost, getFeedPosts, getUserPosts,likePost} from '../controllers/post.controller.js';
 
 router.route("/createpost").post(verifyJWT,upload.single("picture"),createPost);
 router.get("/getfeedposts", verifyJWT, getFeedPosts);
 router.get("/:userId/posts", verifyJWT, getUserPosts);
 router.patch("/:id/like", verifyJWT, likePost);
-
+router.delete("/delete",verifyJWT,deletePost);
 
 export default router
